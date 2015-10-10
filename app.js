9eacade1036fbfa502632a83010b10c41343b815
Shivaji <shivajividhale@gmail.com>
2015-10-10 17:23:45 -0400

    email service paused

--- app.js
+++ app.js
@@ -111,7 +111,7 @@ app.post('/register',function(req, res){
             email.setSubject("Knowledge is valuable");
             email.setHtml("We are glad to have you on board");
 
-            sendgrid.send(email);
+            //sendgrid.send(email);
             console.log("email sent");
             console.log("Added user");
                 res.redirect('/');
